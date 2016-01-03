652f3bec848b38aea9610b920fd5939d05a9f9ca
Benoit Tigeot <benoit@hopsandfork.com>
2016-01-03 16:49:49 +0100

    Fix concurency issue.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,64 @@
 use std::thread;
+use std::sync::{Mutex, Arc};
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep_ms(150);
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} is eating.", self.name);
+
         thread::sleep_ms(1000);
+
         println!("{} is done eating.", self.name);
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Jacques Rancière"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Georgio Agamben"),
-        Philosopher::new("Hannah Arendt"),
-        Philosopher::new("Michel Foucault"),
+        Philosopher::new("Jacques Rancière", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Georgio Agamben", 2, 3),
+        Philosopher::new("Hannah Arendt", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
     ];
 
-    for philosopher in &philosophers {
-        philosopher.eat();
+    let handles: Vec<_> = philosophers.into_iter().map(|philosopher| {
+        let table = table.clone();
+
+        thread::spawn(move || {
+            philosopher.eat(&table);
+        })
+    }).collect();
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
